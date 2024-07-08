7ae678b57cc17a519b0af3c4e6db747601e91914
nkhanna404 <nkhanna@udutu.com>
2024-07-07 17:37:12 -0700

    HR Feed App solution files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HRFeedApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Log to catch any errors while processing a feed.
+            string feedLog = System.Configuration.ConfigurationManager.AppSettings["feedLog"];
+            // Get all the HRFeeds from the database. If any row exists then continue on to read & process the file.
+            List<HRFeeds> feeds = HRFeeds.GetAllFeeds();
+            if (feeds.Count > 0 )
+            {
+                // Loop through all the rows returned from the HRFeeds table.
+                foreach (HRFeeds feed in feeds)
+                {
+                    try
+                    {
+                        CommonUtils.LogMessageToFile(System.DateTime.Now + " , Start Processing CSV for Organization with ID: " + feed.OrganizationID, feedLog);
+                        // Function to Process each feed.
+                        FeedCSVProcessing.ProcessFeed(feed);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log any error to a text file at the time of processing the feed.
+                        CommonUtils.LogMessageToFile(ex.Message, feedLog);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,100 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System;
+
+namespace HRFeedApp
+{
+    static class Utility
+    {
+        private static string _connectionStringPortal
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.ConnectionStrings["LMSDBConnectPortal"].ToString();
+            }
+        }
+
+        public static DataView GetDataFromQueryPortal(string query, CommandType command, Dictionary<string, object> parameters = null)
+        {
+            var ds = new DataSet();
+            var da = new SqlDataAdapter();
+            da.SelectCommand = new SqlCommand();
+            da.SelectCommand.CommandTimeout = 1800;
+            da.SelectCommand.CommandType = command;
+
+            using (var conn = new SqlConnection(_connectionStringPortal))
+            {
+                da.SelectCommand.Connection = conn;
+                da.SelectCommand.CommandText = query;
+
+                if (parameters != null)
+                {
+                    foreach (string key in parameters.Keys)
+                    {
+                        da.SelectCommand.Parameters.AddWithValue(key, parameters[key]);
+                    }
+                }
+
+                da.Fill(ds);
+            }
+            if (ds.Tables.Count > 0)
+            {
+                return ds.Tables[0].DefaultView;
+            }
+
+            return null;
+        }
+
+        public static int SQLNonQuery(string query, Dictionary<string, object> parameters, bool isStoredProcedure)
+        {
+            return SQLNonQuery(query, parameters, isStoredProcedure, out int _);
+        }
+        public static int SQLNonQuery(string query, Dictionary<string, object> parameters, bool isStoredProcedure, out int outputValue)
+        {
+            outputValue = 0; // Default value
+
+            using (SqlConnection conn = new SqlConnection(_connectionStringPortal))
+            {
+                SqlCommand comm = new SqlCommand(query, conn);
+                comm.CommandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+
+                foreach (string key in parameters.Keys)
+                {
+                    if (parameters[key] is SqlParameter sqlParam && sqlParam.Direction == ParameterDirection.Output)
+                    {
+                        comm.Parameters.Add(sqlParam);
+                    }
+                    else
+                    {
+                        if (parameters[key] == null)
+                        {
+                            comm.Parameters.AddWithValue(key, DBNull.Value);
+                        }
+                        else
+                        {
+                            comm.Parameters.AddWithValue(key, parameters[key]);
+                        }
+                    }
+                }
+
+                comm.CommandTimeout = 1800;
+                conn.Open();
+                comm.ExecuteNonQuery();
+
+                // Retrieve the value of the output parameter
+                foreach (SqlParameter param in comm.Parameters)
+                {
+                    if (param.Direction == ParameterDirection.Output)
+                    {
+                        outputValue = (int)param.Value;
+                        break;
+                    }
+                }
+            }
+
+            return outputValue;
+
+        }
+    }
+}
